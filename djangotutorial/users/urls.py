53f97cdc8344281dd84977a7a13f08771ffe97ab
Полина <farutina97@gmail.com>
2025-06-02 08:05:04 +0300

    d

---
+++ djangotutorial/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+from rest_framework.authtoken.views import obtain_auth_token
+
+
+urlpatterns = [
+    path("register/", views.UserRegistrationView.as_view(), name="register"),
+    path("auth-token/", obtain_auth_token, name="obtain-auth-token")
+]
